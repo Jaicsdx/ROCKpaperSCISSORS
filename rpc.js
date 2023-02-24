e57a5ae64958bae5853e640dc64be10ecc9c33d9
“jaicsdx” <jaicsdx@gmail.com>
2023-02-24 00:59:32 -0500

    rpc.js

---
+++ rpc.js
@@ -0,0 +1,77 @@
+
+let playerScore =0;
+let computerScore =0;
+
+function computerPlay()
+{
+
+
+const voucher = ["rock", "paper", "scissors"];
+
+  var rando = voucher[Math.floor(Math.random() * voucher.length)];
+  
+ return rando;
+
+
+}
+
+
+
+function roundOne(playerSelection,computerSelection)
+
+{
+    if 
+        (playerSelection === computerSelection)
+        {
+            return "tie";
+        }
+    
+   
+
+     else if
+     ((playerSelection === "rock" && computerSelection === "scissors")||
+     (playerSelection ==="paper" && computerSelection ==="rock") ||
+     (playerSelection ==="scissors" && computerSelection ==="paper"))
+
+     {
+        playerScore++;
+        return "you won";
+        
+
+     }
+         
+     else 
+     ((playerSelection === "rock" && computerSelection==="paper") ||
+     (playerSelection === "paper" && computerSelection==="scissors")||
+     (playerSelection === "scissors" && computerSelection==="rock"))
+     {
+        computerScore++;
+        return "computer won";
+       
+     }
+     
+     
+     }
+     
+
+
+     function game()
+     {
+        for (let i = 0; i < 3; i++)
+        {
+            const playerSelection = prompt("Enter rock, paper or scissors");
+            const copmuterSelection = computerPlay();
+            console.log(copmuterSelection,playerSelection)
+            console.log(roundOne(playerSelection,copmuterSelection));
+        }
+     
+     if (playerScore > computerScore)
+     { return " you won the game"}
+     else if(playerScore < computerScore)
+     {return "you lost the game"}
+     else if (playerScore === computerScore)
+     {return "you tied the game"}
+     }
+     
+
+     console.log(game());
